e1d2a6f5fc1b23b021272b315366f99f29f2eb77
Pranjal Gaur <gaurpranjal236@gmail.com>
2026-04-15 23:39:29 +0530

    Initial commit - remembero mobile app

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'screens/home_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Task Manager',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const HomeScreen(),
+        // '/second': (context) => const SecondScreen(),
+      },
+    );
+  }
+}
